4b461207bee3900810d564847bc0ba053295f2ab
J.H. Moon <moonjh587@naver.com>
2023-07-27 10:43:13 +0900

    Initial Commit

---
+++ SeSAC Week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC Week3
+//
+//  Created by 문정호 on 2023/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
